bb59bb6270a1c6cb8b73a7c6f185abbb711e4d7e
B Venkata Sreyanth <96385391+bvsreyanth@users.noreply.github.com>
2025-09-26 16:36:35 +0530

    Add files via upload

---
+++ Authentication_And_Autherization/Autherization/Autherization/ExpirationRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Autherization
+{
+    public class ExpirationRequirement : IAuthorizationRequirement
+    {
+        public DateTime ExpiryDate { get; }
+
+        public ExpirationRequirement(DateTime expiryDate)
+        {
+            ExpiryDate = expiryDate;
+        }
+    }
+}
---
+++ Authentication_And_Autherization/Autherization/Autherization/ExpirationRequirementHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Autherization
+{
+    public class ExpirationRequirementHandler : AuthorizationHandler<ExpirationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ExpirationRequirement requirement)
+        {
+            if (DateTime.UtcNow <= requirement.ExpiryDate)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
